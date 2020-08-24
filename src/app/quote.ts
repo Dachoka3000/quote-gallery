53309db45128c782da123fd8e9dca59e72e20c2d
Machoka Daisy <machokadaisy25@gmail.com>
2020-08-24 16:48:36 +0300

    create a component for quote details

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,6 +1,6 @@
 export class Quote {
     showDetails: boolean;
-    constructor(public name:string, public author:string, public quote:string){
+    constructor(public name:string, public author:string, public quotecontent:string){
         this.showDetails= false;
     }
 }
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -11,9 +11,10 @@ export class QuoteComponent implements OnInit {
   quotes: Quote[]= [
     new Quote('Daisy Machoka', 'Maya Angelou', 'Do what you can until you know better. Then when you know better, do better'),
     new Quote('Daisy Machoka', 'Audrey Hepburn', 'Nothing is impossible. The word itself says"Im possible"'),
-    new Quote('Daisy Machoka', 'Lily Tomlin', 'Forgiveness means giving up all hope for a better past'),
+    new Quote('Daisy Machoka', 'Andre Gide', 'It is better to be hated for what you are than to be loved for what you are not.'),
     new Quote('Daisy Machoka', 'Ralph Waldo Emerson', 'It is one of the beautiful compensations in this life that no one can help another without helping himself'),
-    
+    new Quote('Daisy Machoka','Shrii Shrii Anandamutri', 'You are never alone or helpless. The force that guides the stars guides you too.'),
+
   ]
 
   constructor() { }
