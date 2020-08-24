476af6a3463034e4ad0a3357f31a48a27c20d819
Machoka Daisy <machokadaisy25@gmail.com>
2020-08-25 00:56:24 +0300

    add functions to show number of upvotes and downvotes

--- src/app/votes/votes.component.ts
+++ src/app/votes/votes.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-votes',
@@ -6,6 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./votes.component.css']
 })
 export class VotesComponent implements OnInit {
+  voteUp: number=0;
+  voteDown: number =0;
+
+  upvote($index){
+    return this.voteUp +=1
+  }
+  downvote($index){
+    return this.voteDown +=1
+  }
 
   constructor() { }
 
